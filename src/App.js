426cb6fffda0416d58e26e75647a06f9f5b4160e
Zixing(Luke) Li <zl82@cornell.edu>
2017-05-19 13:58:26 -0500

    add landing animation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Navbar from './components/navbar.js'
-import Landing from './components/landing.js'
 import './App.css';
 
 class App extends Component {
@@ -34,3 +33,50 @@ class App extends Component {
 }
 
 export default App;
+
+let pointsArray = document.getElementsByClassName('point');
+ 
+ let animatePoints = function(points) {
+ 
+     var revealFirstPoint = function() {
+         points[0].style.opacity = 1;
+         points[0].style.transform = "scaleX(1) translateY(0)";
+         points[0].style.msTransform = "scaleX(1) translateY(0)";
+         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+     };
+ 
+     var revealSecondPoint = function() {
+         points[1].style.opacity = 1;
+         points[1].style.transform = "scaleX(1) translateY(0)";
+         points[1].style.msTransform = "scaleX(1) translateY(0)";
+         points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
+     };
+ 
+     var revealThirdPoint = function() {
+         points[2].style.opacity = 1;
+         points[2].style.transform = "scaleX(1) translateY(0)";
+         points[2].style.msTransform = "scaleX(1) translateY(0)";
+         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
+     };
+ 
+     revealFirstPoint();
+     revealSecondPoint();
+     revealThirdPoint();
+ 
+ };
+
+ window.onload = function() {
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener("scroll", function(event) {
+             // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+        //  console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+ }
