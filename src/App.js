b250ab89331e45d3fe75a5aab71d25a812950ca2
Zixing(Luke) Li <zl82@cornell.edu>
2017-05-18 23:45:28 -0500

    index page without landing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navbar from './components/navbar.js'
+import Landing from './components/landing.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="landing">
+        <Navbar/>
+          <section className="hero-content"> 
+              <h1 className="hero-title">Turn the music up!</h1>
+          </section>
+          <section className="selling-points container clearfix">
+              <div className="point column third">
+                  <span className="ion-music-note"></span>
+                  <h5 className="point-title">Choose your music</h5>
+                  <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
+              </div>
+              <div className="point column third">
+                  <span className="ion-radio-waves"></span>
+                  <h5 className="point-title">Unlimited, streaming, ad-free</h5>
+                  <p className="point-description">No arbitrary limits. No distractions.</p>
+              </div>
+              <div className="point column third">
+                  <span className="ion-iphone"></span>
+                  <h5 className="point-title">Mobile enabled</h5>
+                  <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+              </div>
+          </section>
       </div>
     );
   }
---
+++ src/components/navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from '../images/bloc_jams_logo.png'
+import './navbar.css';
+
+class Navbar extends Component {
+  render() {
+    return (
+        <nav className="navbar"> 
+         <a href="index.html" className="logo">
+             <img src={logo} alt="bloc jams logo" />
+         </a>
+         <div className="links-container">
+             <a href="collection.html" className="navbar-link">collection</a>
+         </div>
+        </nav>
+    );
+  }
+}
+
+export default Navbar;
