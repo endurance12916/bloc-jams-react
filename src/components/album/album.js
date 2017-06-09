34f86a6cdfbbb6eae3f442974a5948cb018bb127
Zixing(Luke) Li <zl82@cornell.edu>
2017-06-08 22:56:03 -0400

    playbar branc

--- src/components/album/album.js
+++ src/components/album/album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './album.css';
+import PlayerBar from './player_bar.js';
 import cover from '../../assets/images/album_covers/01.png';
 
 const exampleAlbum = [
@@ -91,41 +92,7 @@ class Album extends Component {
                     </table>
                 </main>
 
-                <section className="player-bar">
-                    <div className="container">
-                        <div className="control-group main-controls">
-                            <a className="previous">
-                                <span className="ion-skip-backward"></span>
-                            </a>
-                            <a className="play-pause">
-                                <span className="ion-play"></span>
-                            </a>
-                            <a className="next">
-                                <span className="ion-skip-forward"></span>
-                            </a>
-                        </div>
-                        <div className="control-group currently-playing">
-                            <h2 className="song-name"></h2>
-                            <div className="seek-control">
-                                <div className="seek-bar">
-                                    <div className="fill"></div>
-                                    <div className="thumb"></div>
-                                </div>
-                                <div className="current-time">2:30</div>
-                                <div className="total-time">4:45</div>
-                            </div>
-                            <h2 className="artist-song-mobile"></h2>
-                            <h3 className="artist-name"></h3>
-                        </div>
-                        <div className="control-group volume">
-                            <span className="ion-volume-high icon"></span>
-                            <div className="seek-bar">
-                                <div className="fill"></div>
-                                <div className="thumb"></div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                <PlayerBar />
             </section>
         )
     }
---
+++ src/components/album/player_bar.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import './player_bar.css';
+
+class PlayerBar extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            currentSongNumber: {},
+            songBeingPlayed: {},
+            songBeingPaused: {}
+        }
+    }
+    render() {
+        return (
+            <section className="player-bar">
+                <div className="container">
+                    <div className="control-group main-controls">
+                        <a className="previous">
+                            <span className="ion-skip-backward"></span>
+                        </a>
+                        <a className="play-pause">
+                            <span className="ion-play"></span>
+                        </a>
+                        <a className="next">
+                            <span className="ion-skip-forward"></span>
+                        </a>
+                    </div>
+                    <div className="control-group currently-playing">
+                        <h2 className="song-name"></h2>
+                        <div className="seek-control">
+                            <div className="seek-bar">
+                                <div className="fill"></div>
+                                <div className="thumb"></div>
+                            </div>
+                            <div className="current-time">2:30</div>
+                            <div className="total-time">4:45</div>
+                        </div>
+                        <h2 className="artist-song-mobile"></h2>
+                        <h3 className="artist-name"></h3>
+                    </div>
+                    <div className="control-group volume">
+                        <span className="ion-volume-high icon"></span>
+                        <div className="seek-bar">
+                            <div className="fill"></div>
+                            <div className="thumb"></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default PlayerBar;
