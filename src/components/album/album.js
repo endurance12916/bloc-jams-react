223ce465015376de9c953d6a6fffb00d1fbe6244
Zixing(Luke) Li <zl82@cornell.edu>
2017-06-24 23:45:56 -0400

    all functions work

--- src/components/album/album.js
+++ src/components/album/album.js
@@ -17,8 +17,6 @@ class Album extends Component {
             songBeingPaused: {},
             currentSoundFile:{},
             currentVolume:80,
-            currentTime:"0:00",
-            totalTime:"0:00"
         }
     }
 
@@ -57,23 +55,41 @@ class Album extends Component {
     };
 
     mouseLeave = (event) => {
-        this.setState({
-            currentSongNumber: {}
-        })
+        this.setState({currentSongNumber: {}})
     };
 
     mouseClick = (song, event) => {
-        let sound = this.setSong(song);
+        this.clickSongChangeState(song);
+    }
 
-        sound.isPaused() ?
-            (this.setState({
-                songBeingPaused: this.state.currentSongObject.number
-            }), sound.play(), this.setState({
-                songBeingPlayed: this.state.currentSongNumber
-            })) :
-            (sound.pause(), this.setState({
-                songBeingPaused: this.state.currentSongNumber
-            }))
+    clickSongChangeState = (song) => {
+        let sound = this.setSong(song);
+        if (!_.isEmpty(sound)) {
+            sound.isPaused() ?
+                (this.setState({
+                    songBeingPaused: this.state.currentSongNumber,
+                }), sound.play(), this.setState({
+                    songBeingPlayed: this.state.currentSongNumber
+                })) :
+                (sound.pause(), this.setState({
+                    songBeingPaused: this.state.currentSongNumber
+                }))
+        }
+    }
+
+    clickButtonChangeState = (song) => {
+        let sound = this.setSong(song);
+        if (!_.isEmpty(sound)) {
+            sound.isPaused() ?
+                (this.setState({
+                    songBeingPaused: song.number
+                }), sound.play(), this.setState({
+                    songBeingPlayed: song.number
+                })) :
+                (sound.pause(), this.setState({
+                    songBeingPaused: song.number
+                }))
+        }
     }
 
     setSong = (song) => {
@@ -83,59 +99,31 @@ class Album extends Component {
         if (!_.isEmpty(this.state.currentSoundFile) && this.state.currentSongObject !== song) {
             // pause the previous song, then set new song
             this.state.currentSoundFile.pause();
-
-            this.setState({
-                currentSongObject: song
-            })
-
             const sound = new buzz.sound(song.audioUrl, {
                 preload: true
             })
-
-            let _this = this;
-            sound.bind("timeupdate", function() {
-                let timer = buzz.toTimer(this.getTime());
-                let ratio = sound.getTime() / sound.getDuration();
-                let position = _this.timeline.offsetWidth * ratio;
-                _this.positionThumb(position);
-
-                // is this the best way? constantly changing states, would this eat a lot of cpu/memory?
-                _this.setState({currentTime:_this.setCurrentTimeInPlayerBar(sound.getTime())})
-                _this.setState({totalTime:_this.setTotalTimeInPlayerBar(sound.getDuration())})
-            });
-            
-            this.setState({currentSoundFile: sound})
+            // is this the best way?
+            this.child.timeUpdate(sound)
+            this.setState({currentSoundFile: sound, currentSongObject: song})
             this.setVolume(sound, this.state.currentVolume)
-
             return sound
 
         } else if (this.state.currentSongObject === song) {
             // if same song 
+            this.setState({currentSongObject:song})
             return this.state.currentSoundFile
 
         } else {
             // if first time playing
-            this.setState({currentSongObject: song})
-
             const sound = new buzz.sound(song.audioUrl, {
                 preload: true
             })
 
-            let _this = this;
-            sound.bind("timeupdate", function() {
-                let timer = buzz.toTimer(this.getTime());
-                let ratio = sound.getTime() / sound.getDuration();
-                let position = _this.timeline.offsetWidth * ratio;
-                _this.positionThumb(position);
+            // is this the best way?
+            this.child.timeUpdate(sound)
 
-                // is this the best way? constantly changing states, would this eat a lot of cpu/memory?
-                _this.setState({currentTime:_this.setCurrentTimeInPlayerBar(sound.getTime())})
-                _this.setState({totalTime:_this.setTotalTimeInPlayerBar(sound.getDuration())})
-            });
-
-            this.setState({currentSoundFile: sound})
+            this.setState({currentSoundFile: sound, currentSongObject: song})
             this.setVolume(sound, this.state.currentVolume)
-
             return sound
         }
     }
@@ -146,51 +134,17 @@ class Album extends Component {
         }
     }
 
-    // Below are player_bar functions. To move to player_bar.js when all of them function properly
-    playerBarClick = (song, event) => {
-        let sound = this.setSong(this.state.currentSongObject)
-        if (!_.isEmpty(sound)) {
-            sound.isPaused() ?
-                (this.setState({
-                    songBeingPaused: this.state.currentSongObject.number
-                }), sound.play(), this.setState({
-                    songBeingPlayed: this.state.currentSongObject.number
-                })) :
-                (sound.pause(), this.setState({
-                    songBeingPaused: this.state.currentSongObject.number
-                }))
-        }
-    }
-
-    playerBarPlayButtonContent() {
-        if (_.isEmpty(this.state.currentSoundFile)) {
-            return (<span className="ion-play"></span>)
-        } else if (this.state.currentSoundFile.isPaused()){
-            return (<span className="ion-play"></span>)
-        } else {
-            return (<span className="ion-pause"></span>)
-        }
-    }
-
-    nextSong() {
-        let newSong = this.setSong(exampleAlbum[this.state.currentSongObject.number-1+1]);
+    // the two functions below will return error if not bind
+    nextSong = (newSong) => {
         this.setState({songBeingPlayed:this.state.songBeingPlayed+1, songBeingPaused:{}});
-        newSong.play();
+        let sound = this.setSong(newSong);
+        sound.play();
     }
 
-    previousSong() {
-        let newSong = this.setSong(exampleAlbum[this.state.currentSongObject.number-1-1]);
+    previousSong = (newSong) => {
         this.setState({songBeingPlayed:this.state.songBeingPlayed-1, songBeingPaused:{}});
-        newSong.play();
-    }
-
-    // timeline bar functions
-    setCurrentTimeInPlayerBar(currentTime) {
-        return this.filterTimeCode(parseInt(currentTime));
-    }
-
-    setTotalTimeInPlayerBar(totalTime) {
-        return this.filterTimeCode(parseInt(totalTime));
+        let sound = this.setSong(newSong);
+        sound.play();
     }
 
     filterTimeCode(timeInSeconds) {
@@ -198,78 +152,6 @@ class Album extends Component {
         let seconds = parseFloat(timeInSeconds) - minutes * 60;
         return seconds < 10 ? (minutes + `:0` + seconds) : (minutes + ':' + seconds);
     }
-
-    // mouse functions for the timeline
-    mouseDown = (e) => {
-        window.addEventListener('mousemove', this.mouseMove);
-        window.addEventListener('mouseup', this.mouseUp);
-    };
-
-    mouseUp = (e) => {
-        window.removeEventListener('mousemove', this.mouseMove);
-        window.removeEventListener('mouseup', this.mouseUp);
-    };
-
-    mouseMove = (e) => {
-        this.positionThumb(e.pageX-this.container.offsetLeft);
-        let sound = this.state.currentSoundFile;
-        sound.setTime(((e.pageX-this.container.offsetLeft) / this.timeline.offsetWidth) * sound.getDuration())
-    };
-
-    positionThumb = (position) => {
-        let timelineWidth = this.timeline.offsetWidth - this.thumb.offsetWidth;
-        let thumbLeft = position - this.timeline.offsetLeft;
-        if (thumbLeft >= 0 && thumbLeft <= timelineWidth) {
-            this.thumb.style.left = thumbLeft + "px";
-            this.fill.style.width = this.thumb.style.left;
-        }
-        if (thumbLeft < 0) {
-            this.thumb.style.left = "0px";
-            this.fill.style.width = this.thumb.style.left;
-        }
-        if (thumbLeft > timelineWidth) {
-            this.thumb.style.left = timelineWidth + "px";
-            this.fill.style.width = this.thumb.style.left;
-        }
-    };
-
-    // mouse functions for the volume
-    mouseDownVol = (e) => {
-        window.addEventListener('mousemove', this.mouseMoveVol);
-        window.addEventListener('mouseup', this.mouseUpVol);
-    };
-
-    mouseUpVol = (e) => {
-        window.removeEventListener('mousemove', this.mouseMoveVol);
-        window.removeEventListener('mouseup', this.mouseUpVol);
-    };
-
-    mouseMoveVol = (e) => {
-        this.positionThumbVol(e.pageX);
-        let sound = this.state.currentSoundFile;
-        let offsetX = e.pageX - this.volBar.offsetLeft;
-        let vol = offsetX/this.volBar.offsetWidth * 100;
-        if (vol>100) {vol = 100} else if (vol<0) {vol = 0}
-        this.setVolume(sound, vol)
-        this.setState({currentVolume:vol})
-    };
-
-    positionThumbVol = (position) => {
-        let thumbVolLeft = position - this.volBar.offsetLeft;
-        let volBarWidth = this.volBar.offsetWidth;
-        if (thumbVolLeft >= 0 && thumbVolLeft <= volBarWidth) {
-            this.thumbVol.style.left = thumbVolLeft + "px";
-            this.fillVol.style.width = this.thumbVol.style.left;
-        }
-        if (thumbVolLeft < 0) {
-            this.thumbVol.style.left = + "0px";
-            this.fillVol.style.width = this.thumbVol.style.left;
-        }
-        if (thumbVolLeft > volBarWidth) {
-            this.thumbVol.style.left = volBarWidth + "px";
-            this.fillVol.style.width = this.thumbVol.style.left;
-        }
-    };
 
     render() {
         return (
@@ -292,54 +174,19 @@ class Album extends Component {
                     </table>
                 </main>
 
-                {/*<PlayerBar
+                <PlayerBar
+                ref={instance => { this.child = instance }}
                 currentSongObject={this.state.currentSongObject}
-                currentSongNumber={this.state.currentSongNumber}
-                songBeingPlayed={this.state.songBeingPlayed}
-                songBeingPaused={this.state.songBeingPaused}
-                
+                currentSoundFile={this.state.currentSoundFile}
                 currentVolume={this.state.currentVolume}
-                setSong={this.setSong}
+                clickButtonChangeState={this.clickButtonChangeState}
                 mouseClick={this.mouseClick}
                 nextSong={this.nextSong}
                 previousSong={this.previousSong}
                 setVolume={this.setVolume}
-                />*/}
-                <section className="player-bar">
-                    <div className="container">
-                        <div className="control-group main-controls">
-                            <a className="previous" onMouseUp={this.previousSong.bind(this)}>
-                                <span className="ion-skip-backward"></span>
-                            </a>
-                            <a className="play-pause" onMouseUp={this.playerBarClick.bind(this)}>
-                                {this.playerBarPlayButtonContent()}
-                            </a>
-                            <a className="next" onMouseUp={this.nextSong.bind(this)}>
-                                <span className="ion-skip-forward"></span>
-                            </a>
-                        </div>
-                        <div className="control-group currently-playing" ref={(container) => { this.container = container }}>
-                            <h2 className="song-name">{this.state.currentSongObject.title}</h2>
-                            <div className="seek-control">
-                                <div className="seek-bar" onClick={this.mouseMove} ref={(timeline) => { this.timeline = timeline }} >
-                                    <div className="fill" ref={(fill) => { this.fill = fill }}></div>
-                                    <div className="thumb" onMouseDown={this.mouseDown} ref={(thumb) => { this.thumb = thumb }} ></div>
-                                </div>
-                                <div className="current-time">{this.state.currentTime}</div>
-                                <div className="total-time">{this.state.totalTime}</div>
-                            </div>
-                            <h2 className="artist-song-mobile"></h2>
-                            <h3 className="artist-name"></h3>
-                        </div>
-                        <div className="control-group volume" ref={(containerVol) => { this.containerVol = containerVol }}>
-                            <span className="ion-volume-high icon"></span>
-                            <div className="seek-bar" onClick={this.mouseMoveVol} ref={(volBar) => { this.volBar = volBar }}>
-                                <div className="fill" ref={(fillVol) => { this.fillVol = fillVol }} ></div>
-                                <div className="thumbVol" onMouseDown={this.mouseDownVol} ref={(thumbVol) => { this.thumbVol = thumbVol }} ></div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                filterTimeCode={this.filterTimeCode}
+                />
+
             </section>
         )
     }
--- src/components/album/player_bar.js
+++ src/components/album/player_bar.js
@@ -1,136 +1,171 @@
-// currently putting this in album.js. Will separate after all buttons function
-
 import React, { Component } from 'react';
 import './player_bar.css';
 import exampleAlbum from '../fixtures/fixtures.js';
 import buzz from 'buzz';
+import _ from 'lodash'; 
 
 class PlayerBar extends Component {
     constructor(props){
         super(props);
 
         this.state = {
-            
+            currentTime:"0:00",
+            totalTime:"0:00"
         }
     }
-    setVolume(sound, volume) {
-        if (sound) {
-            sound.setVolume(volume);
+
+    timeUpdate = (sound) => {
+        let _this = this;
+        sound.bind("timeupdate", function() {
+            let timer = buzz.toTimer(this.getTime());
+            let ratio = sound.getTime() / sound.getDuration();
+            let position = _this.timeline.offsetWidth * ratio;
+            _this.positionThumb(position);
+
+            // is this the best way? constantly changing states, would this eat a lot of cpu/memory?
+            _this.setState({currentTime:_this.setCurrentTimeInPlayerBar(sound.getTime())})
+            _this.setState({totalTime:_this.setTotalTimeInPlayerBar(sound.getDuration())})
+        });
+    }
+
+    playerBarClick = (event) => {
+        this.props.clickButtonChangeState(this.props.currentSongObject)
+    }
+
+    playerBarPlayButtonContent() {
+        if (_.isEmpty(this.props.currentSoundFile)) {
+            return (<span className="ion-play"></span>)
+        } else if (this.props.currentSoundFile.isPaused()){
+            return (<span className="ion-play"></span>)
+        } else {
+            return (<span className="ion-pause"></span>)
         }
     }
-    mouseClick(){
-        Object.getOwnPropertyNames(this.props.songBeingPlayed).length === 0
-        // this.props.songBeingPlayed==={}
-        ? this.props.setSong(this.props.songBeingPaused)
-        : this.props.setSong(this.props.songBeingPlayed)
+
+    nextSong() {
+        let newSong = exampleAlbum[this.props.currentSongObject.number-1+1];
+        this.props.nextSong(newSong);
     }
 
-    // Below are codes copied from jquery. To update code.
-    // updateSeekBarWhileSongPlays() {
-    //     if (this.props.currentSoundFile) {
-    //         // #10
-    //         this.props.currentSoundFile.bind('timeupdate', function (event) {
-    //             // #11
-    //             let seekBarFillRatio = this.getTime() / this.getDuration();
-    //             let $seekBar = $('.seek-control .seek-bar');
+    previousSong() {
+        let newSong = exampleAlbum[this.props.currentSongObject.number-1-1];
+        this.props.previousSong(newSong);
+    }
 
-    //             updateSeekPercentage($seekBar, seekBarFillRatio);
-    //             setCurrentTimeInPlayerBar(this.getTime());
-    //         });
-    //     }
-    // };
+    // timeline bar functions
+    setCurrentTimeInPlayerBar(currentTime) {
+        return this.props.filterTimeCode(parseInt(currentTime));
+    }
 
-    // updateSeekPercentage($seekBar, seekBarFillRatio) {
-    //     let offsetXPercent = seekBarFillRatio * 100;
-    //     // #1
-    //     offsetXPercent = Math.max(0, offsetXPercent);
-    //     offsetXPercent = Math.min(100, offsetXPercent);
+    setTotalTimeInPlayerBar(totalTime) {
+        return this.props.filterTimeCode(parseInt(totalTime));
+    }
 
-    //     // #2
-    //     let percentageString = offsetXPercent + '%';
-    //     $seekBar.find('.fill').width(percentageString);
-    //     $seekBar.find('.thumb').css({
-    //         left: percentageString
-    //     });
-    // };
+    // mouse functions for the timeline
+    mouseDown = (e) => {
+        window.addEventListener('mousemove', this.mouseMove);
+        window.addEventListener('mouseup', this.mouseUp);
+    };
 
-    // setupSeekBars() {
-    //     // #6
-    //     let $seekBars = $('.player-bar .seek-bar');
+    mouseUp = (e) => {
+        window.removeEventListener('mousemove', this.mouseMove);
+        window.removeEventListener('mouseup', this.mouseUp);
+    };
 
-    //     $seekBars.click(function (event) {
-    //         let offsetX = event.pageX - $(this).offset().left;
-    //         let barWidth = $(this).width();
-    //         let seekBarFillRatio = offsetX / barWidth;
+    mouseMove = (e) => {
+        this.positionThumb(e.pageX-this.container.offsetLeft);
+        let sound = this.props.currentSoundFile;
+        sound.setTime(((e.pageX-this.container.offsetLeft) / this.timeline.offsetWidth) * sound.getDuration())
+    };
 
-    //         if ($(this).parent().attr('class') == 'seek-control') {
-    //             seek(seekBarFillRatio * this.props.currentSoundFile.getDuration());
-    //         } else {
-    //             setVolume(seekBarFillRatio * 100);
-    //         }
+    positionThumb = (position) => {
+        let timelineWidth = this.timeline.offsetWidth - this.thumb.offsetWidth;
+        let thumbLeft = position - this.timeline.offsetLeft;
+        if (thumbLeft >= 0 && thumbLeft <= timelineWidth) {
+            this.thumb.style.left = thumbLeft + "px";
+            this.fill.style.width = this.thumb.style.left;
+        }
+        if (thumbLeft < 0) {
+            this.thumb.style.left = "0px";
+            this.fill.style.width = this.thumb.style.left;
+        }
+        if (thumbLeft > timelineWidth) {
+            this.thumb.style.left = timelineWidth + "px";
+            this.fill.style.width = this.thumb.style.left;
+        }
+    };
 
-    //         updateSeekPercentage($(this), seekBarFillRatio);
-    //     });
+    // mouse functions for the volume
+    mouseDownVol = (e) => {
+        window.addEventListener('mousemove', this.mouseMoveVol);
+        window.addEventListener('mouseup', this.mouseUpVol);
+    };
 
-    //     $seekBars.find('.thumb').mousedown(function (event) {
+    mouseUpVol = (e) => {
+        window.removeEventListener('mousemove', this.mouseMoveVol);
+        window.removeEventListener('mouseup', this.mouseUpVol);
+    };
 
-    //         let $seekBar = $(this).parent();
+    mouseMoveVol = (e) => {
+        this.positionThumbVol(e.pageX);
+        let sound = this.props.currentSoundFile;
+        let offsetX = e.pageX - this.volBar.offsetLeft;
+        let vol = offsetX/this.volBar.offsetWidth * 100;
+        if (vol>100) {vol = 100} else if (vol<0) {vol = 0}
+        this.props.setVolume(sound, vol)
+        this.setState({currentVolume:vol})
+    };
 
-    //         $(document).bind('mousemove.thumb', function (event) {
-    //             let offsetX = event.pageX - $seekBar.offset().left;
-    //             let barWidth = $seekBar.width();
-    //             let seekBarFillRatio = offsetX / barWidth;
-
-    //             if ($seekBar.parent().attr('class') == 'seek-control') {
-    //                 seek(seekBarFillRatio * this.props.currentSoundFile.getDuration());
-    //             } else {
-    //                 setVolume(seekBarFillRatio);
-    //             }
-
-    //             updateSeekPercentage($seekBar, seekBarFillRatio);
-    //         });
-
-    //         // #10
-    //         $(document).bind('mouseup.thumb', function () {
-    //             $(document).unbind('mousemove.thumb');
-    //             $(document).unbind('mouseup.thumb');
-    //         });
-    //     });
-    // };
-
+    positionThumbVol = (position) => {
+        let thumbVolLeft = position - this.volBar.offsetLeft;
+        let volBarWidth = this.volBar.offsetWidth;
+        if (thumbVolLeft >= 0 && thumbVolLeft <= volBarWidth) {
+            this.thumbVol.style.left = thumbVolLeft + "px";
+            this.fillVol.style.width = this.thumbVol.style.left;
+        }
+        if (thumbVolLeft < 0) {
+            this.thumbVol.style.left = + "0px";
+            this.fillVol.style.width = this.thumbVol.style.left;
+        }
+        if (thumbVolLeft > volBarWidth) {
+            this.thumbVol.style.left = volBarWidth + "px";
+            this.fillVol.style.width = this.thumbVol.style.left;
+        }
+    };
+    
     render() {
         return (
             <section className="player-bar">
                 <div className="container">
                     <div className="control-group main-controls">
-                        <a className="previous">
+                        <a className="previous" onMouseUp={this.previousSong.bind(this)}>
                             <span className="ion-skip-backward"></span>
                         </a>
-                        <a className="play-pause" onMouseUp={this.mouseClick.bind(this)}>
-                            <span className="ion-play"></span>
+                        <a className="play-pause" onMouseUp={this.playerBarClick.bind(this)}>
+                            {this.playerBarPlayButtonContent()}
                         </a>
-                        <a className="next">
+                        <a className="next" onMouseUp={this.nextSong.bind(this)}>
                             <span className="ion-skip-forward"></span>
                         </a>
                     </div>
-                    <div className="control-group currently-playing">
+                    <div className="control-group currently-playing" ref={(container) => { this.container = container }}>
                         <h2 className="song-name">{this.props.currentSongObject.title}</h2>
                         <div className="seek-control">
-                            <div className="seek-bar">
-                                <div className="fill"></div>
-                                <div className="thumb"></div>
+                            <div className="seek-bar" onClick={this.mouseMove} ref={(timeline) => { this.timeline = timeline }} >
+                                <div className="fill" ref={(fill) => { this.fill = fill }}></div>
+                                <div className="thumb" onMouseDown={this.mouseDown} ref={(thumb) => { this.thumb = thumb }} ></div>
                             </div>
-                            <div className="current-time">2:30</div>
-                            <div className="total-time">4:45</div>
+                            <div className="current-time">{this.state.currentTime}</div>
+                            <div className="total-time">{this.state.totalTime}</div>
                         </div>
                         <h2 className="artist-song-mobile"></h2>
                         <h3 className="artist-name"></h3>
                     </div>
-                    <div className="control-group volume">
+                    <div className="control-group volume" ref={(containerVol) => { this.containerVol = containerVol }}>
                         <span className="ion-volume-high icon"></span>
-                        <div className="seek-bar">
-                            <div className="fill"></div>
-                            <div className="thumb"></div>
+                        <div className="seek-bar" onClick={this.mouseMoveVol} ref={(volBar) => { this.volBar = volBar }}>
+                            <div className="fill" ref={(fillVol) => { this.fillVol = fillVol }} ></div>
+                            <div className="thumbVol" onMouseDown={this.mouseDownVol} ref={(thumbVol) => { this.thumbVol = thumbVol }} ></div>
                         </div>
                     </div>
                 </div>
