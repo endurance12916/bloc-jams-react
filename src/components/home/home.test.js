e3fd2bf05b0bbd0909a92e199f05b37bb05e421b
Zixing(Luke) Li <zl82@cornell.edu>
2017-07-05 13:54:14 -0400

    home.test.js erros

--- src/components/home/home.test.js
+++ src/components/home/home.test.js
@@ -14,13 +14,38 @@ it('renders a snapshot', () => {
 });
 
 // Why does this one return error, even though the first test (renders without crashing) passes?
+// FAIL  src/components/home/home.test.js
+//   ● renders default text
+
+//     expect(received).toEqual(expected)
+
+//     Expected value to equal:
+//       true
+//     Received:
+//       false
+
+//       at Object.<anonymous>.it (src/components/home/home.test.js:20:42)
+//       at process._tickCallback (internal/process/next_tick.js:109:7)
+
 it('renders default text', () => {
   const wrapper = mount(<Home />)
   const expectedNode = shallow(<DefaultText />)
   expect((wrapper).contains(expectedNode)).toEqual(true);
 })
 
+
 // This test doesn't work...How to test a scrolling event?
+//  ● renders additional texts after scrolling
+
+//     expect(received).toEqual(expected)
+
+//     Expected value to equal:
+//       true
+//     Received:
+//       false
+
+//       at Object.<anonymous>.it (src/components/home/home.test.js:26:75)
+//       at process._tickCallback (internal/process/next_tick.js:109:7)
 it('renders additional texts after scrolling', () => {
   const scrolling = mount(<Home />).simulate('scroll');
   expect(scrolling.contains(ScrollText)).toEqual(true);
